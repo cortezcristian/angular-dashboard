2b00e1ae40de42c05da1136cd052e6a18004aca4
Cristian Cortez <cortez.cristian@gmail.com>
2015-08-24 17:26:15 -0300

    Adding Functionality

--- app/scripts/controllers/proveedores.js
+++ app/scripts/controllers/proveedores.js
@@ -8,10 +8,38 @@
  * Controller of the angularDashboardApp
  */
 angular.module('angularDashboardApp')
-  .controller('ProveedoresCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ProveedoresCtrl', function($scope, $http){
+        console.log("cargo proveedores controller");
+        $scope.proveedores = [];
+
+        $scope.cargarProveedores = function(prov) {
+            $http.post('http://localhost:9001/proveedores', {
+                nombre: prov.nombre.$modelValue
+            })
+            .success(function(res){
+                console.log("creado");
+                $scope.listarProveedores();
+            });
+
+        }
+
+        $scope.borrarProveedor = function(idProv) {
+            $http.delete('http://localhost:9001/proveedores/'+idProv)
+            .success(function(res){
+                console.log("creado");
+                $scope.listarProveedores();
+            });
+
+        }
+
+        $scope.listarProveedores = function(){
+            $http.get('http://localhost:9001/proveedores').then(function(res){
+                console.log("success!", res);
+                $scope.proveedores = res.data;
+            }, function(){
+                console.log("error!");
+            });
+        }
+
+        $scope.listarProveedores();
   });
